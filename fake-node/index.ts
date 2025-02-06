a84001336e83092b9cb8f7e63c556d119285236c
speedydelete <speedykorok777@gmail.com>
2025-02-05 20:25:49 -0500

    slightly fix up fake-node package

--- fake-node/index.ts
+++ fake-node/index.ts
@@ -1,22 +1,29 @@
 
-import * as module_assert from 'assert';
+import module_assert from 'assert';
 import * as module_buffer from 'buffer';
+// @ts-ignore
 import * as module_path from 'path';
 import * as module_punycode from 'punycode';
 import * as module_querystring from 'querystring';
+// @ts-ignore
 import * as module_readline from 'readline';
+// @ts-ignore
 import * as module_stream from 'stream';
+// @ts-ignore
 import * as module_string_decoder from 'string_decoder';
 import * as module_test from 'test';
+// @ts-ignore
 import * as module_url from 'url';
+// @ts-ignore
 import * as module_util from 'util';
 import WEB_ONLY_GLOBALS from './web_only_globals.json';
 
 
 // todo: properly implement eval
-const ES_GLOBALS = {Infinity, NaN, undefined, eval, isFinite, isNaN, parseFloat, parseInt, decodeURI, decodeURIComponent, encodeURI, encodeURIComponent, escape, unescape, Object, Function, Boolean, Symbol, Error, AggregateError, EvalError, RangeError, ReferenceError, SyntaxError, TypeError, URIError, Number, BigInt, Math, Date, String, RegExp, Array, Int8Array, Uint8Array, Uint8ClampedArray, Int16Array, Uint16Array, Int32Array, Uint32Array, BigInt64Array, BigUint64Array, Float32Array, Float64Array, Map, Set, WeakMap, WeakSet, ArrayBuffer, SharedArrayBuffer, DataView, Atomics, JSON, WeakRef, FinalizationRegistry, Promise, Reflect, Proxy, Intl};
-
-const NODE_AND_WEB_GLOBALS = {AbortController, Blob, ByteLengthQueuingStrategy, atob, BroadcastChannel, btoa, clearInterval, clearTimeout, CloseEvent, CompressionStream, console, CountQueuingStrategy, Crypto, crypto, CryptoKey, CustomEvent, DecompressionStream, Event, EventSource, EventTarget, fetch, FormData, Headers, localStorage, MessageChannel, MessageEvent, MessagePort, Navigator, navigator, PerformanceEntry, PerformanceMark, PerformanceMeasure, PerformanceObserver, PerformanceObserverEntryList, performance, queueMicrotask, ReadableByteStreamController, Response, Request, sessionStorage, setInterval, setTimeout, Storage, structuredClone, SubtleCrypto, DOMException, TextDecoder, TextDecoderStream, TextEncoder, TextEncoderStream, TransformStreamDefaultController, URL, URLSearchParams, WebAssembly, WebSocket, WritableStream, WritableStreamDefaultController, WritableStreamDefaultWriter};
+const DEFAULT_GLOBALS = {Infinity, NaN, undefined, eval, isFinite, isNaN, parseFloat, parseInt, decodeURI, decodeURIComponent, encodeURI, encodeURIComponent, escape, unescape, Object, Function, Boolean, Symbol, Error, AggregateError, EvalError, RangeError, ReferenceError, SyntaxError, TypeError, URIError, Number, BigInt, Math, Date, String, RegExp, Array, Int8Array, Uint8Array, Uint8ClampedArray, Int16Array, Uint16Array, Int32Array, Uint32Array, BigInt64Array, BigUint64Array, Float32Array, Float64Array, Map, Set, WeakMap, WeakSet, ArrayBuffer, SharedArrayBuffer, DataView, Atomics, JSON, WeakRef, FinalizationRegistry, Promise, Reflect, Proxy, Intl, AbortController, Blob, ByteLengthQueuingStrategy, atob, BroadcastChannel, btoa, clearInterval, clearTimeout, CloseEvent, CompressionStream, console, CountQueuingStrategy, Crypto, crypto, CryptoKey, CustomEvent, DecompressionStream, Event, EventSource, EventTarget, fetch, FormData, Headers, localStorage, MessageChannel, MessageEvent, MessagePort, Navigator, navigator, PerformanceEntry, PerformanceMark, PerformanceMeasure, PerformanceObserver, PerformanceObserverEntryList, performance, queueMicrotask, ReadableByteStreamController, Response, Request, sessionStorage, setInterval, setTimeout, Storage, structuredClone, SubtleCrypto, DOMException, TextDecoder, TextDecoderStream, TextEncoder, TextEncoderStream, TransformStreamDefaultController, URL, URLSearchParams, WebAssembly, WebSocket, WritableStream, WritableStreamDefaultController, WritableStreamDefaultWriter};
+for (const name of WEB_ONLY_GLOBALS) {
+    Object.defineProperty(DEFAULT_GLOBALS, name, {get: () => {throw new ReferenceError(`${name} is not defined`)}});
+}
 
 
 export class Process {
@@ -25,18 +32,40 @@ export class Process {
     pid: number;
     argv: string[] = [];
     argv0: string = '';
-    path?: string;
+    module: false | string;
     code: string;
+    path: string;
 
-    constructor(fakeNode: FakeNode, {path, code}: {path?: string, code?: string}) {
+    constructor(fakeNode: FakeNode, {path = '', code, module}: {path?: string, code: string, module?: false | string}) {
         this.fakeNode = fakeNode;
+        this.pid = fakeNode.nextPid;
         fakeNode.nextPid++;
+        fakeNode.processes.set(this.pid, this);
+        this.code = code;
+        this.path = path;
+        this.module = module ?? false;
+    }
+
+    run(): void {
+        let code: string;
+        if (this.module) {
+            code = `with(${this.fakeNode.globalName}.getGlobals(${this.pid})){__fakeNode_process__.fakeNode.modules.set('${this.module},(function(){${this.code};return module.exports;})());}`;
+        } else {
+            code = `with(${this.fakeNode.globalName}.getGlobals(${this.pid})){(function(){${this.code}})();}`;
+        }
+        let elt = document.createElement('script');
+        elt.textContent = code;
+        document.body.appendChild(elt);
     }
 
 }
 
 
 export class FakeNode {
+
+    static nextId: number = 0;
+    id: number;
+    globalName: string;
     
     modules: Map<string, any> = new Map();
     builtinModules: Map<string, any> = new Map<string, any>([
@@ -61,60 +90,59 @@ export class FakeNode {
     window: Window = window;
 
     constructor() {
-        this.globals = {require: this.require, Buffer: module_buffer.Buffer};
-        Object.assign(this.globals, ES_GLOBALS);
-        for (const name of WEB_ONLY_GLOBALS) {
-            Object.defineProperty(this.globals, name, {get: () => {throw new ReferenceError(`${name} is not defined`)}});
-        }
-        Object.assign(this.globals, NODE_AND_WEB_GLOBALS);
+        this.id = FakeNode.nextId;
+        FakeNode.nextId++;
+        this.globalName = '__fakeNode_' + this.id + '__';
+        // @ts-ignore
+        globalThis[this.globalName] = this;
+        this.globals = Object.create(DEFAULT_GLOBALS);
+        Object.assign(this.globals, {require: this.require, Buffer: module_buffer.Buffer});
     }
 
     require(module: string): any {
+        if (module.startsWith('fake-node:')) {
+            module = module.slice('fake-node:'.length);
+        } else if (module.startsWith('node:')) {
+            module = module.slice('node:'.length);
+        }
         if (this.modules.has(module)) {
             return this.modules.get(module);
         } else if (this.builtinModules.has(module)) {
             return this.builtinModules.get(module);
         } else {
-            throw new Error(`cannot find module '${name}'`);
+            throw new Error(`cannot find module '${module}'`);
         }
     }
 
-    setup({path, argv, module}: {path?: string, argv?: string[], module?: boolean}): object {
+    getGlobals(pid: number): object {
+        const process = this.processes.get(pid);
+        if (process === undefined) {
+            throw new TypeError(`nonexistent PID in FakeNode.getGlobals call: ${pid}. If you do not know why this occured, it is probably a bug in fake-node. Please report it at https://github.com/speedydelete/fake-node/issues.`);
+        }
         let scope: {[key: string]: any} = Object.create(this.globals);
         scope.global = scope;
         scope.globalThis = scope;
-        let process = new Process(this, path, argv);
         scope.__fakeNode_process__ = process;
-        if (path !== undefined) {
-            const splitPath = path.split('/');
-            scope.__dirname = splitPath.slice(0, -1).join('/');
-            scope.__filename = splitPath[splitPath.length - 1];
+        if (process.path !== '') {
+            const pathParts = process.path.split('/');
+            scope.__dirname = pathParts.slice(0, -1).join('/');
+            scope.__filename = pathParts[pathParts.length - 1];
         }
-        if (argv !== undefined) {
-            
-        }
-        if (module !== undefined) {
-
+        if (process.module !== false) {
+            scope.module = {
+                exports: {}
+            };
+            scope.exports = scope.module.exports;
         }
         return scope;
     }
 
-    run(code: string) {
-        new Process()
-    }
-
-    run(code: string, path?: string): void {
-        code = `with(__fakeNode__.setup(${JSON.stringify(path)}){(function(){${code}})();}`;
-        let elt = document.createElement('script');
-        elt.textContent = code;
-        document.body.appendChild(elt);
+    run(code: string): void {
+        (new Process(this, {code, path: '/'})).run();
     }
 
     addModule(name: string, code: string): void {
-        code = `with(window.__fakeNode__.setup()){__fakeNode__.modules.set('${name}',(function(){${code};return module.exports;})();window.__fakeNode_cleanup();}`;
-        let elt = document.createElement('script');
-        elt.textContent = code;
-        document.body.appendChild(elt);
+        (new Process(this, {code, path: '/', module: name})).run();
     }
 
 }
--- modules/types/index.d.ts
+++ modules/types/index.d.ts
@@ -1,30 +1,39 @@
 
-export namespace FakeNode {
-
+export declare class Process {
+    fakeNode: FakeNode;
+    pid: number;
+    argv: string[];
+    argv0: string;
+    module: boolean;
+    code: string;
+    path: string;
+    constructor(fakeNode: FakeNode, {path, code, module}: {path?: string, code: string, module?: boolean});
+    run(): void;
 }
 
-declare class FakeNode {
+export declare class FakeNode {
+    static nextId: number;
+    id: number;
+    globalName: string;
     modules: Map<string, any>;
     builtinModules: Map<string, any>;
-    globals: {
-        [key: string]: any;
-    };
-    argvs: Map<number, string[]>;
-    argv0s: Map<number, string>;
-    execArgvs: Map<number, string[]>;
-    execArgv0s: Map<number, string>;
+    globals: {[key: string]: any};
+    processes: Map<number, Process>;
     nextPid: number;
     window: Window;
     constructor();
     require(module: string): any;
-    setup({ path, argv, module }: {
-        path?: string;
-        argv?: string[];
-        module?: boolean;
-    }): object;
-    run(code: string, path?: string): void;
+    getGlobals(pid: number): object;
+    run(code: string): void;
     addModule(name: string, code: string): void;
 }
+
+declare namespace FN {
+    var FakeNode: FakeNode;
+    var Process: Process;
+}
+
+export default FN;
 
 declare global {
     var __fakeNode_process__: Process;
