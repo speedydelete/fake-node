d11ef561b06533e513b2aa580091c893cf386214
speedydelete <speedykorok777@gmail.com>
2025-02-10 17:54:16 -0500

    fix bug with __fakeNode__ not being defined

--- src/fs.ts
+++ src/fs.ts
@@ -1,59 +1,55 @@
 
 /// <reference path="./in_fake_node.d.ts" />
-import {callbackify, type TypedArray} from './util';
+import {type TypedArray} from './util';
 import {dirname, basename, resolve} from './path';
 import {Buffer, type BufferEncoding} from './buffer';
 import {constants, parsePathArg, Directory, FileSystem} from './_fs';
 import type {PathArg, ModeArg, DataArg, TimeArg, Flag, Stats, BigIntStats, StatFs, BigIntStatFs} from './_fs';
-
-
-const fs = __fakeNode__.fs;
-
 
 export {constants} from './_fs';
 
 export {Stats, BigIntStats, StatFs, BigIntStatFs} from './_fs';
 
 export function accessSync(path: PathArg, mode: number = constants.F_OK) {
-    fs.get(path).access(mode);
+    __fakeNode__.fs.get(path).access(mode);
 }
 
 export function appendFileSync(path: PathArg, data: DataArg, {encoding = 'utf8', mode = 0o666, flag = 'a'}: {encoding?: BufferEncoding, mode: number, flag: string}) {
-    fs.getRegular(path).append(data, encoding);
+    __fakeNode__.fs.getRegular(path).append(data, encoding);
 }
 
 export function chmodSync(path: PathArg, mode: ModeArg): void {
-    fs.get(path).chmod(mode);
+    __fakeNode__.fs.get(path).chmod(mode);
 }
 
 export function chownSync(path: PathArg, uid: number, gid: number): void {
-    fs.get(path).chown(uid, gid);
+    __fakeNode__.fs.get(path).chown(uid, gid);
 }
 
 export function closeSync(fd: number): void {
-    fs.fileDescriptors[fd] = null;
+    __fakeNode__.fs.fileDescriptors[fd] = null;
 }
 
 export function copyFileSync(src: PathArg, dest: PathArg, mode: number = 0): void {
-    if ((mode & constants.COPYFILE_EXCL) === constants.COPYFILE_EXCL && fs.exists(dest)) {
+    if ((mode & constants.COPYFILE_EXCL) === constants.COPYFILE_EXCL && __fakeNode__.fs.exists(dest)) {
         throw new TypeError(`${dest} exists`);
     }
     if ((mode & constants.COPYFILE_FICLONE_FORCE) === constants.COPYFILE_FICLONE_FORCE) {
         throw new TypeError('fake-node does not support copy-on-write');
     }
-    fs.getRegular(dest).write(fs.getRegular(src).read());
+    __fakeNode__.fs.getRegular(dest).write(__fakeNode__.fs.getRegular(src).read());
 }
 
 export function existsSync(path: PathArg): boolean {
-    return fs.exists(path);
+    return __fakeNode__.fs.exists(path);
 }
 
 export function fchmodSync(fd: number, mode: ModeArg): void {
-    fs.getfd(fd).chmod(mode);
+    __fakeNode__.fs.getfd(fd).chmod(mode);
 }
 
 export function fchownsync(fd: number, uid: number, gid: number): void {
-    fs.getfd(fd).chown(uid, gid);
+    __fakeNode__.fs.getfd(fd).chown(uid, gid);
 }
 
 export function fdatasyncSync(fd: number): void {
@@ -61,7 +57,7 @@ export function fdatasyncSync(fd: number): void {
 }
 
 export function fstatSync(fd: number): Stats {
-    return fs.getfd(fd).stat();
+    return __fakeNode__.fs.getfd(fd).stat();
 }
 
 export function fsyncSync(fd: number): void {
@@ -69,12 +65,12 @@ export function fsyncSync(fd: number): void {
 }
 
 export function ftruncateSync(fd: number, len: number): void {
-    let file = fs.getfdRegular(fd);
+    let file = __fakeNode__.fs.getfdRegular(fd);
     file.data = file.data.slice(0, len);
 }
 
 export function futimesSync(fd: number, atime: TimeArg, ctime: TimeArg): void {
-    fs.getfd(fd).utimes(atime, ctime);
+    __fakeNode__.fs.getfd(fd).utimes(atime, ctime);
 }
 
 export function globSync(pattern: string | string[]): string[] {
@@ -82,19 +78,19 @@ export function globSync(pattern: string | string[]): string[] {
 }
 
 export function lchmodSync(path: PathArg, mode: number): void {
-    fs.lget(path).chmod(mode);
+    __fakeNode__.fs.lget(path).chmod(mode);
 }
 
 export function lchownSync(path: PathArg, uid: number, gid: number): void {
-    fs.lget(path).chown(uid, gid);
+    __fakeNode__.fs.lget(path).chown(uid, gid);
 }
 
 export function lutimesSync(path: PathArg, atime: TimeArg, mtime: TimeArg): void {
-    fs.lget(path).utimes(atime, mtime);
+    __fakeNode__.fs.lget(path).utimes(atime, mtime);
 }
 
 export function linkSync(existingPath: PathArg, newPath: PathArg): void {
-    fs.getDir(dirname(parsePathArg(existingPath))).link(basename(parsePathArg(newPath)), fs.get(existingPath));
+    __fakeNode__.fs.getDir(dirname(parsePathArg(existingPath))).link(basename(parsePathArg(newPath)), __fakeNode__.fs.get(existingPath));
 }
 
 export function lstatSync(path: PathArg, {bigint, throwIfNoEntry}: {bigint?: false, throwIfNoEntry: false}): Stats;
@@ -102,11 +98,11 @@ export function lstatSync(path: PathArg, {bigint, throwIfNoEntry}: {bigint?: fal
 export function lstatSync(path: PathArg, {bigint, throwIfNoEntry}: {bigint?: true, throwIfNoEntry: false}): BigIntStats;
 export function lstatSync(path: PathArg, {bigint, throwIfNoEntry}: {bigint?: true, throwIfNoEntry: true}): BigIntStats | undefined;
 export function lstatSync(path: PathArg, {bigint = false, throwIfNoEntry = false}: {bigint?: boolean, throwIfNoEntry?: boolean} = {}): Stats | BigIntStats | undefined {
-    if (!throwIfNoEntry && !fs.exists(path)) {
+    if (!throwIfNoEntry && !__fakeNode__.fs.exists(path)) {
         return undefined;
     }
     // @ts-ignore // why is it doing this
-    return fs.lget(path).stat(bigint);
+    return __fakeNode__.fs.lget(path).stat(bigint);
 }
 
 export function mkdirSync(path: PathArg, options: {recursive?: boolean, mode?: ModeArg} | ModeArg = 0o777): void {
@@ -119,7 +115,7 @@ export function mkdirSync(path: PathArg, options: {recursive?: boolean, mode?: M
         recursive = options.recursive ?? false;
         mode = options.mode ?? 0o777;
     }
-    fs.mkdir(path, recursive, mode);
+    __fakeNode__.fs.mkdir(path, recursive, mode);
 }
 
 export function mkdtempSync(prefix: PathArg, options: {encoding?: string} | string = 'utf8'): void {
@@ -131,7 +127,7 @@ export function opendirSync(path: PathArg, {encoding = 'utf8', bufferSize = 32, 
 }
 
 export function openSync(path: PathArg, flags: Flag, mode: ModeArg = 'r'): number {
-    return fs.open(path, flags, mode);
+    return __fakeNode__.fs.open(path, flags, mode);
 }
 
 export function readdirSync(path: PathArg, options: {encoding?: string, withFileTypes?: boolean, recursive?: boolean} | string = 'utf8') {
@@ -142,7 +138,7 @@ export function readFileSync(path: PathArg, options: {encoding?: null | 'buffer'
 export function readFileSync(path: PathArg, options: {encoding: BufferEncoding, flag?: string} | BufferEncoding): string;
 export function readFileSync(path: PathArg, options: {encoding?: BufferEncoding | null | 'buffer', flag?: string} | BufferEncoding | 'buffer' = {encoding: null, flag: 'r'}): string | Buffer {
     // @ts-ignore // why is it doing this
-    return fs.getRegular(path).read(typeof options === 'string' ? options : options.encoding ?? 'buffer');
+    return __fakeNode__.fs.getRegular(path).read(typeof options === 'string' ? options : options.encoding ?? 'buffer');
 }
 
 export function readlinkSync(path: PathArg, options: {encoding?: string} | string = 'utf8'): string | Buffer {
@@ -151,7 +147,7 @@ export function readlinkSync(path: PathArg, options: {encoding?: string} | strin
 
 export function readSync(fd: number, buffer: Buffer | TypedArray | DataView, offset: number, length: number, position: number | bigint | null = null): number {
     position = Number(position ?? 0);
-    const data = fs.getfdRegular(fd).read('uint8array', offset, length);
+    const data = __fakeNode__.fs.getfdRegular(fd).read('uint8array', offset, length);
     if (buffer instanceof DataView) {
         for (let i = position; i < data.length; i++) {
             buffer.setUint8(i, data[i]);
@@ -175,22 +171,22 @@ realpathSync.native = realpathSync;
 export function renameSync(oldPath: PathArg, newPath: PathArg): void {
     const parsedOldPath = parsePathArg(oldPath);
     const parsedNewPath = parsePathArg(newPath);
-    const file = fs.getDir(dirname(parsedOldPath)).unlink(basename(parsedOldPath));
-    fs.getDir(dirname(parsedNewPath)).link(basename(parsedNewPath), file);
+    const file = __fakeNode__.fs.getDir(dirname(parsedOldPath)).unlink(basename(parsedOldPath));
+    __fakeNode__.fs.getDir(dirname(parsedNewPath)).link(basename(parsedNewPath), file);
 }
 
 export function rmdirSync(path: PathArg): void {
-    const file = fs.get(path);
+    const file = __fakeNode__.fs.get(path);
     if (!(file instanceof Directory)) {
         throw new TypeError(`cannot remove directory ${path}: is not a directory`);
     } else if (!(file.size === 0)) {
         throw new TypeError(`cannot remove directory ${path}: is not empty`);
     }
-    fs.unlink(path);
+    __fakeNode__.fs.unlink(path);
 }
 
 export function rmSync(path: PathArg): void {
-    fs.unlink(path);
+    __fakeNode__.fs.unlink(path);
 }
 
 export function statSync(path: PathArg, {bigint, throwIfNoEntry}: {bigint?: false, throwIfNoEntry: false}): Stats;
@@ -198,17 +194,17 @@ export function statSync(path: PathArg, {bigint, throwIfNoEntry}: {bigint?: fals
 export function statSync(path: PathArg, {bigint, throwIfNoEntry}: {bigint?: true, throwIfNoEntry: false}): BigIntStats;
 export function statSync(path: PathArg, {bigint, throwIfNoEntry}: {bigint?: true, throwIfNoEntry: true}): BigIntStats | undefined;
 export function statSync(path: PathArg, {bigint = false, throwIfNoEntry = false}: {bigint?: boolean, throwIfNoEntry?: boolean} = {}): Stats | BigIntStats | undefined {
-    if (!throwIfNoEntry && !fs.exists(path)) {
+    if (!throwIfNoEntry && !__fakeNode__.fs.exists(path)) {
         return undefined;
     }
     // @ts-ignore // why is it doing this
-    return fs.get(path).stat(bigint);
+    return __fakeNode__.fs.get(path).stat(bigint);
 }
 
 export function statfsSync(path: PathArg, {bigint}: {bigint?: false}): StatFs;
 export function statfsSync(path: PathArg, {bigint}: {bigint?: true}): BigIntStatFs;
 export function statfsSync(path: PathArg, {bigint = false}: {bigint?: boolean} = {}): StatFs | BigIntStatFs {
-    let file = fs.get(path);
+    let file = __fakeNode__.fs.get(path);
     if (!(file instanceof FileSystem)) {
         throw new TypeError(`cannot get fs stat for ${path}: is not a file system`);
     }
@@ -217,18 +213,18 @@ export function statfsSync(path: PathArg, {bigint = false}: {bigint?: boolean} =
 }
 
 export function symlinkSync(target: PathArg, path: PathArg): void {
-    fs.symlink(target, path);
+    __fakeNode__.fs.symlink(target, path);
 }
 
 export function truncateSync(path: PathArg, len: number = 0): void {
-    let file = fs.getRegular(path);
+    let file = __fakeNode__.fs.getRegular(path);
     file.data = file.data.slice(0, len);
 }
 
 export function unlinkSync(path: PathArg): void {
-    fs.unlink(path);
+    __fakeNode__.fs.unlink(path);
 }
 
 export function utimesSync(path: PathArg, atime: TimeArg, mtime: TimeArg) {
-    fs.get(path).utimes(atime, mtime);
+    __fakeNode__.fs.get(path).utimes(atime, mtime);
 }
